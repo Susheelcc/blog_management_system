a3484af1ea8068ab931848114b7f018789a3d04a
Susheel <Susheelmokashimath903@gmail.com>
2026-01-03 02:54:51 +0530

    added javaDoc comments

--- src/main/java/com/blogapp/BlogManagementApplication.java
+++ src/main/java/com/blogapp/BlogManagementApplication.java
@@ -3,9 +3,18 @@ package com.blogapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Main entry point for the Blog Management Application.
+ * This Spring Boot application provides REST APIs for managing blog users and operations.
+ */
 @SpringBootApplication
 public class BlogManagementApplication {
 
+	/**
+	 * Main method to start the Spring Boot application.
+	 *
+	 * @param args command line arguments
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(BlogManagementApplication.class, args);
 	}
